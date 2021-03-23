005462de28b8c6b11fe0edf47696377865a072f1
Vicky Lee <vicky.winkay.lee@gmail.com>
2021-03-23 10:33:40 +0000

    use chance names for demo

--- karaoke-web-ui/src/App.tsx
+++ karaoke-web-ui/src/App.tsx
@@ -5,7 +5,7 @@ import * as api from './api';
 import Loading from './Loading';
 import { Player } from './Player/Player';
 import { Playlist, QueuedSong } from './Playlist/Playlist';
-import { v4 as uuidv4 } from 'uuid';
+import { Chance } from 'chance';
 
 import { User } from './api';
 import { SocketContext } from './SocketContext/SocketContext';
@@ -14,7 +14,7 @@ const API_ENDPOINT = 'http://localhost:5000/';
 
 const App = () => {
   const socket = React.useContext(SocketContext);
-  const [username, setUserName] = React.useState(uuidv4());
+  const [username, setUserName] = React.useState(Chance().first());
   const [ready, setReady] = React.useState(false);
   const [error, setError] = React.useState(false);
   const [data, setData] = React.useState('');
